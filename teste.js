e9d02d08304a67df3f2b07b6163069fada208835
otavioasnhack <150282392+otavioasnhack@users.noreply.github.com>
2023-12-15 17:09:39 -0300

    adicionados mais exercicios resolvidos

--- teste.js
+++ teste.js
@@ -1,4 +1,18 @@
-function DNAtoRNA(dna) {
+/*function DNAtoRNA(dna) {
     return dna.replaceAll('T','U') // Substitui as strings passadas por parametros
 }
 console.log(DNAtoRNA('TTTTTTT'))
+
+function stringToArray(string){
+return string.split(' ') // dividi uma string e converte em um array de palavras.
+}
+console.log(stringToArray('I LOVE YOU'))*/
+
+/*function paperwork(n, m) {
+  let mult = n * m 
+    if (n<0 || m<0) {
+    return 0
+  }
+  return mult
+}
+console.log(paperwork(5, 7))*/
