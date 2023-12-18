3da1211ffcfdea81a101dd1432d8b3453e7bb65f
otavioasnhack <150282392+otavioasnhack@users.noreply.github.com>
2023-12-18 18:17:37 -0300

    Mais exercicios realizados

--- teste.js
+++ teste.js
@@ -16,3 +16,29 @@ console.log(stringToArray('I LOVE YOU'))*/
   return mult
 }
 console.log(paperwork(5, 7))*/
+/*function maps(x){
+  const double = (x)=> x*2
+  return x.map((item)=>item * 2) // metodo e arrow function para dobrar itens de um array
+  }
+  console.log(maps([4,8,10,12]))*/
+  /*const stringToNumber = function(str){
+    return Number.parseInt(str) // Transforma uma string em um número
+  }
+  console.log(typeof stringToNumber('1234'))*/
+  /*function makeNegative(num) {
+    if (num==0) {
+     return 0
+    }
+    return -Math.abs(num) // Transformando número positivo em negativo
+  }
+  console.log(makeNegative(17))
+  console.log(makeNegative(-5))
+  console.log(makeNegative(0))*/
+  /*function removeChar(str){
+    return str.substring(1, str.length - 1) //Removendo o primeiro e o ultimo caractere de uma string
+   }
+   console.log(removeChar('otavio'))
+   console.log(removeChar('mariano'))
+   console.log(removeChar('pedro'))*/
+ 
+
