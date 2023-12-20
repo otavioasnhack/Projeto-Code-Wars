ebec8104bfdee68fb1be8a3163d6884cc1948101
otavioasnhack <150282392+otavioasnhack@users.noreply.github.com>
2023-12-20 17:12:45 -0300

    Desafios realizados quarta 20/12

--- teste.js
+++ teste.js
@@ -31,14 +31,36 @@ console.log(paperwork(5, 7))*/
     }
     return -Math.abs(num) // Transformando número positivo em negativo
   }
-  console.log(makeNegative(17))
-  console.log(makeNegative(-5))
+  console.log(makeNegative(100))
+  console.log(makeNegative(-20))
   console.log(makeNegative(0))*/
   /*function removeChar(str){
     return str.substring(1, str.length - 1) //Removendo o primeiro e o ultimo caractere de uma string
    }
    console.log(removeChar('otavio'))
    console.log(removeChar('mariano'))
-   console.log(removeChar('pedro'))*/
- 
+   console.log(removeChar('pedro'))
+   console.log(removeChar('diego'))
+   console.log(removeChar('isaias'))*/
+   /*function removeExclamationMarks(s) {
+    return s.replace(/!/g, ' '); // Remove todas as ocorrências da palavra dentro das barras
+  }
+  console.log(removeExclamationMarks('!Hello World!'))*/
+  /*function booleanToString(b){
+    return b.toString()*/ // converte o valor booleano fornecido em sua representação de string.
 
+    /*function lovefunc(flower1, flower2){
+      if ((flower1 %2==0 && flower2 %2==1) || (flower2 %2==0 && flower1 %2==1)) { // Testando condições de veracidade
+        return true
+      }
+        return false
+    }
+    console.log(lovefunc(13, 3))*/
+    
+    /*class SmallestIntegerFinder {
+      findSmallestInt(args) {
+        return Math.min(...args)
+      }
+    }
+    const classInstance = new SmallestIntegerFinder()
+    console.log(classInstance.findSmallestInt([2, 5, 7, 23, 37, 49, 1, 3]))*/
