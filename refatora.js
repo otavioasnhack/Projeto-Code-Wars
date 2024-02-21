a6086f03d23468c11ef65f84c5b805f14894a981
otavioasnhack <150282392+otavioasnhack@users.noreply.github.com>
2024-02-21 18:09:16 -0300

    Desafios feitos no instituto 21/02/24

--- refatora.js
+++ refatora.js
@@ -1,24 +1,20 @@
-//  function squareSumWithForEach(numbers) {
-//     let sum = 0
-//     numbers.forEach((currentItem)=>{
-//         sum+=currentItem**2
-//     })
-//      return sum
-//      }
-//      console.log(squareSumWithForEach([2,3,4]));
-//  ------------------------------------ // ---------------------------------------- //
+// const numbers = []
+// const limit = 10
+// for (let index = 0; index < limit; index+=1) {
+//     numbers.push(index)
+// }
+//Array.from({length:limit}).forEach((_, index) => {numbers.push(index)})
+// const numbers = Array.from({length:limit}).map((_, index) => index)
+//console.log(numbers);
+// for (let index = 0; index < limit; index++) {
+//     if (index % 2 == 0) {
+//         numbers.push(index)
+//     }
+// }
+// const pair = numbers.filter((itemCurrent) => itemCurrent % 2 == 0)
+// console.log(pair);
 
-//  function findAverage(array) {
-//  let sum = 0
-//  if (array.length == 0) {
-//     return 0
-//  }
-//  array.forEach((currentItem) => {
-//     sum+=currentItem
-//  });
-//  return sum/array.length
-//  }
-//  console.log(findAverage([1,2,3,4]))
-//  console.log(findAverage([1,2,3]))
-//  console.log(findAverage([]))
- //  ------------------------------------ // ---------------------------------------- //
+// const numbersList = [2, 3, 4, 5, 6, 10, 9]
+// const oddNumbers = numbersList.filter((CurrentItem) => CurrentItem % 2 == 0)
+// const squareNumbers = oddNumbers.map((CurrentItem) => CurrentItem ** 2)
+// console.log(squareNumbers);
