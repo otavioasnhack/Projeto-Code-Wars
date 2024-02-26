b6d6d0f2e56f258fef71a2010f8e316a26235c5f
otavioasnhack <150282392+otavioasnhack@users.noreply.github.com>
2024-02-26 15:19:19 -0300

    atualizando

--- src/teste.js
+++ src/teste.js
@@ -1,69 +1,69 @@
-/*function DNAtoRNA(dna) {
-    return dna.replaceAll('T','U') // Substitui as strings passadas por parametros
-}
-console.log(DNAtoRNA('TTTTTTT'))
+// /*function DNAtoRNA(dna) {
+//     return dna.replaceAll('T','U') // Substitui as strings passadas por parametros
+// }
+// console.log(DNAtoRNA('TTTTTTT'))
 
-function stringToArray(string){
-return string.split(' ') // dividi uma string e converte em um array de palavras.
-}
-console.log(stringToArray('I LOVE YOU'))*/
+// function stringToArray(string){
+// return string.split(' ') // dividi uma string e converte em um array de palavras.
+// }
+// console.log(stringToArray('I LOVE YOU'))*/
 
-/*function paperwork(n, m) {
-  let mult = n * m 
-    if (n<0 || m<0) {
-    return 0
-  }
-  return mult
-}
-console.log(paperwork(5, 7))*/
-/*function maps(x){
-  const double = (x)=> x*2
-  return x.map((item)=>item * 2) // metodo e arrow function para dobrar itens de um array
-  }
-  console.log(maps([4,8,10,12]))*/
-  /*const stringToNumber = function(str){
-    return Number.parseInt(str) // Transforma uma string em um número
-  }
-  console.log(typeof stringToNumber('1234'))*/
-  /*function makeNegative(num) {
-    if (num==0) {
-     return 0
-    }
-    return -Math.abs(num) // Transformando número positivo em negativo
-  }
-  console.log(makeNegative(100))
-  console.log(makeNegative(-20))
-  console.log(makeNegative(0))*/
-  /*function removeChar(str){
-    return str.substring(1, str.length - 1) //Removendo o primeiro e o ultimo caractere de uma string
-   }
-   console.log(removeChar('otavio'))
-   console.log(removeChar('mariano'))
-   console.log(removeChar('pedro'))
-   console.log(removeChar('diego'))
-   console.log(removeChar('isaias'))*/
-   /*function removeExclamationMarks(s) {
-    return s.replace(/!/g, ' '); // Remove todas as ocorrências da palavra dentro das barras
-  }
-  console.log(removeExclamationMarks('!Hello World!'))*/
-  /*function booleanToString(b){
-    return b.toString()*/ // converte o valor booleano fornecido em sua representação de string.
+// /*function paperwork(n, m) {
+//   let mult = n * m 
+//     if (n<0 || m<0) {
+//     return 0
+//   }
+//   return mult
+// }
+// console.log(paperwork(5, 7))*/
+// /*function maps(x){
+//   const double = (x)=> x*2
+//   return x.map((item)=>item * 2) // metodo e arrow function para dobrar itens de um array
+//   }
+//   console.log(maps([4,8,10,12]))*/
+//   /*const stringToNumber = function(str){
+//     return Number.parseInt(str) // Transforma uma string em um número
+//   }
+//   console.log(typeof stringToNumber('1234'))*/
+//   /*function makeNegative(num) {
+//     if (num==0) {
+//      return 0
+//     }
+//     return -Math.abs(num) // Transformando número positivo em negativo
+//   }
+//   console.log(makeNegative(100))
+//   console.log(makeNegative(-20))
+//   console.log(makeNegative(0))*/
+//   /*function removeChar(str){
+//     return str.substring(1, str.length - 1) //Removendo o primeiro e o ultimo caractere de uma string
+//    }
+//    console.log(removeChar('otavio'))
+//    console.log(removeChar('mariano'))
+//    console.log(removeChar('pedro'))
+//    console.log(removeChar('diego'))
+//    console.log(removeChar('isaias'))*/
+//    /*function removeExclamationMarks(s) {
+//     return s.replace(/!/g, ' '); // Remove todas as ocorrências da palavra dentro das barras
+//   }
+//   console.log(removeExclamationMarks('!Hello World!'))*/
+//   /*function booleanToString(b){
+//     return b.toString()*/ // converte o valor booleano fornecido em sua representação de string.
 
-    /*function lovefunc(flower1, flower2){
-      if ((flower1 %2==0 && flower2 %2==1) || (flower2 %2==0 && flower1 %2==1)) { // Testando condições de veracidade
-        return true
-      }
-        return false
-    }
-    console.log(lovefunc(13, 3))*/
+//     /*function lovefunc(flower1, flower2){
+//       if ((flower1 %2==0 && flower2 %2==1) || (flower2 %2==0 && flower1 %2==1)) { // Testando condições de veracidade
+//         return true
+//       }
+//         return false
+//     }
+//     console.log(lovefunc(13, 3))*/
     
-    /*class SmallestIntegerFinder {
-      findSmallestInt(args) {
-        return Math.min(...args)
-      }
-    }
-    const classInstance = new SmallestIntegerFinder()
-    console.log(classInstance.findSmallestInt([2, 5, 7, 23, 37, 49, 1, 3]))*/
+//     /*class SmallestIntegerFinder {
+//       findSmallestInt(args) {
+//         return Math.min(...args)
+//       }
+//     }
+//     const classInstance = new SmallestIntegerFinder()
+//     console.log(classInstance.findSmallestInt([2, 5, 7, 23, 37, 49, 1, 3]))*/
 
     // function past(h, m, s){
     // let hora = 3600 * h
@@ -535,3 +535,5 @@ console.log(paperwork(5, 7))*/
 // console.log(uniqueInOrder([1,2,2,3,3]));
 // console.log(uniqueInOrder('AAAABBBCCDAABBB'));
 // console.log(uniqueInOrder('ABBCcAD'));
+
+// Atualizando o github
